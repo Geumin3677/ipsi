789a0b08f520ecca75ef90700d0709d2a93944b3
ABELA <91381965+Geumin3677@users.noreply.github.com>
2023-08-25 00:58:41 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,5 @@
-/** @type {import('next').NextConfig} */
-const withPlugins = require("next-compose-plugins");
-const withPWA = require("next-pwa");
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
 
-const nextConfig = {
-	reactStrictMode: true,
-  output: 'export',
-  basePath: "/"
-};
-
-module.exports = withPlugins(
-	[
-		[
-			withPWA,
-			{
-				pwa: {
-					dest: "public",
-					subdomainPrefix: "/"
-				},
-			},
-		],
-		// 추가 플러그인 작성
-	],
-	nextConfig
-);
+module.exports = withPWA()
